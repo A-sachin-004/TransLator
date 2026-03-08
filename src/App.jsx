66b8525c6bdfd3d7c09505c34c1820c8df0c7c9b
Sachin <115621448+A-sachin-004@users.noreply.github.com>
2026-03-09 01:44:10 +0530

    Update API endpoint for video submission

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ function App() {
     setVideoUrl('');
 
     try {
-      const response = await axios.post('https://c7fb-34-125-88-180.ngrok-free.app/submit', {
+      const response = await axios.post('https://490c-34-145-9-253.ngrok-free.app/submit', {
         youtube_url: youtubeUrl,
         target_lang: targetLang
       });
