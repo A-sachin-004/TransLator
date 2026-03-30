5e865fa9875bcaf67946531b0d72158ad64957f7
Sachin <115621448+A-sachin-004@users.noreply.github.com>
2026-03-30 13:37:26 +0530

    Update BASE_URL to new ngrok endpoint

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const BASE_URL = 'https://aca6-34-138-211-45.ngrok-free.app';
+const BASE_URL = 'https://e784-34-186-19-255.ngrok-free.app';
 
 function App() {
   const [youtubeUrl, setYoutubeUrl] = useState('');
